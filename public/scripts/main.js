3eaf52d03eb05a26b566026e059fa7595dbd9cbf
RIGAUDIE David <drigaudie@yahoo.fr>
2019-10-24 11:52:05 +0200

    Lazy loading video

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,19 +12,44 @@ if (bowser.msie) {
   $('.browser').removeClass('hide');
 }
 
-$(document).ready(() => {
-  new LazyLoad();
+// $(document).ready(() => {
+new LazyLoad();
 
-  if (document.cookie.indexOf('cookie_enabled') == -1) {
-    $('.header__cookies').removeClass('hide');
-  }
-  $('#js-cookie__validate').click(() => {
-    if (document.cookie.indexOf('cookie_enabled') == -1) {
-      document.cookie = 'cookie_enabled=true; expires=Fri, 3 Aug 2100 20:47:11 UTC; path=/';
-      $('.header__cookies').addClass('hide');
-    }
+var youtube = document.querySelectorAll(".youtube");
+
+for (var i = 0; i < youtube.length; i++) {
+
+  var source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed + "/sddefault.jpg";
+
+  var image = new Image();
+  image.src = source;
+  image.addEventListener("load", function () {
+    youtube[i].appendChild(image);
+  }(i));
+
+  youtube[i].addEventListener("click", function () {
+
+    var iframe = document.createElement("iframe");
+
+    iframe.setAttribute("height", "315");
+    iframe.setAttribute("allowfullscreen", "");
+    iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
+
+    this.innerHTML = "";
+    this.appendChild(iframe);
   });
+};
 
-  addFavicon();
-
+if (document.cookie.indexOf('cookie_enabled') == -1) {
+  $('.header__cookies').removeClass('hide');
+}
+$('#js-cookie__validate').click(() => {
+  if (document.cookie.indexOf('cookie_enabled') == -1) {
+    document.cookie = 'cookie_enabled=true; expires=Fri, 3 Aug 2100 20:47:11 UTC; path=/';
+    $('.header__cookies').addClass('hide');
+  }
 });
+
+addFavicon();
+
+// });
